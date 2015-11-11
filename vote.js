e844d8dec5c02a7b32c0653ed2aefaf10f579e2c
Thom Nienow <elstad.nienow@gmail.com>
2015-11-10 17:01:09 -0800

    added chart, does not update

--- vote.js
+++ vote.js
@@ -81,6 +81,7 @@ var tracker = {
 
 var content = tracker.generateRandom();
 var content2 = tracker.generateRandom();
+
 var click = document.getElementById("imgOne");
 var click2 = document.getElementById("imgTwo");
 var clickButton = document.getElementById("newImages");
@@ -90,4 +91,92 @@ click.addEventListener('click', tracker.handleClick);
 click2.addEventListener('click', tracker.handleClick);
 clickButton.addEventListener('click', tracker.handleButton);
 tracker.loadImages();
+
+//---------------chart----
+var ctx = document.getElementById("myChart").getContext("2d");
+
+var makeLabels = function(){
+  var labels = [];
+  for (i=0; i < images.length; i++) {
+    labels.push(images[i].name);
+  }
+  return labels;
+};
+
+var makeData = function() {
+  var data = [];
+  for (i=0; i < images.length; i++) {
+    data.push(images[i].votes);
+  }
+  return data;
+};
+
+
+
+
+
+var data = {
+    labels: makeLabels(),
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: makeData(),
+        }
+      ]
+    };
+
+var myBarChart = new Chart(ctx).Bar(data, {
+
+    ///Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 1,
+
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
+
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.4,
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 4,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+});
 
